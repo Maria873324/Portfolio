380cf05204580b0fb798a69d93337783bab88c6a
Maria Salimi <maryasalimi132@gmail.com>
2025-09-26 20:29:28 +0300

    Update server.js to fix root route for deploy

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname)));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/:page', (req, res) => {
+  const page = req.params.page;
+  res.sendFile(path.join(__dirname, `${page}.html`));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+
 });
